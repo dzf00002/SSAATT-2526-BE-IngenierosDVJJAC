fb5b03d5375f2b32ffb2a5da3ef6490b3d70b764
dzf00002 <dzf00002@red.ujaen.es>
2026-05-11 16:30:53 +0200

    datos iniciales

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const dns = require("node:dns");
 const { MongoClient, ObjectId } = require("mongodb");
 
 //Datos del servicio 
-
+const VERSION = "1.0";
+const SERVICE_NAME = "API Amor y Lentejas (IngenierosDVJJAC)";
+const SERVICE_PORT = 8081;
 
