9250e5bd3295d00e0012d96071dfce104da6dcc5
jlm00107 <jlm00107@red.ujaen.es>
2026-05-11 16:33:58 +0200

    Codigos de estado y constantes de base de datos

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ const VERSION = "1.0";
 const SERVICE_NAME = "API Amor y Lentejas (IngenierosDVJJAC)";
 const SERVICE_PORT = 8081;
 
+//Códigos estado HTTP
+const STATUS_OK = 200;
+const STATUS_CREATED = 201;
+const STATUS_BADFORMAT = 400;
+const STATUS_UNAUTHORIZED = 401;
+const STATUS_FORBIDDEN = 403;
+const STATUS_NOTFOUND = 404;
+const STATUS_SERVER_ERROR = 500;
+
+
+// Constantes de la base de datos
+const DB_URL = "mongodb://localhost:27017/";
+const DB_NAME = "amorylentejas";
+const DB_USERS_COLLECTION = "users";
+const DB_TURNOS_COLLECTION = "turnos";
+
