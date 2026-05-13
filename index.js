95e5c2ec715e24353b130b622977677c9070de7d
jarg0030 <jarg0030@red.ujaen.es>
2026-05-13 12:37:31 +0200

    Comentado

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require("express");
-const os = require("node:os");
-const dns = require("node:dns");
-const { MongoClient, ObjectId } = require("mongodb");
+
+
+/*Importacion de librerias necesarias*/
+const express = require("express"); //Permite crear el servidor, en concreto para definir rutas con PUT, GET o DELETE
+const os = require("node:os"); //Obtiene información del SO y nombre de la maquina
+const dns = require("node:dns"); //Obtiene la IP asociada al nombre del equipo
+const { MongoClient, ObjectId } = require("mongodb"); //Permite conectarse a mongo y crear un identificador
 
 //Datos del servicio 
 const VERSION = "1.0";
@@ -18,58 +21,71 @@ const STATUS_NOTFOUND = 404;
 const STATUS_SERVER_ERROR = 500;
 
 
-// Constantes de la base de datos
+// Constantes de la base de datos. Define los datos de conexión con Mongo
 const DB_URL = "mongodb://localhost:27017/";
-const DB_NAME = "amorylentejas";
+const DB_NAME = "amorylentejas";                //Base de datos usada
 const DB_USERS_COLLECTION = "voluntarios";
 const DB_TURNOS_COLLECTION = "turnos";
 
 // Express 
-const app = new express();
-app.use(express.json()); 
+const app = new express(); //Crea la aplicacion del servidor
+app.use(express.json());  //Permite que Express pueda leer las peticiones en JSON
 app.use((req, res, next) => {
   console.log("[SERVIDOR] Petición entrante: " + req.method + " " + req.path);
   next();
-});
+});  //Muestra cada peticion que llega al servidor
+
 
 //Endpoint post/login
 app.post("/login", (req, res) => {
 
   // Comprueba que la petición incluye un cuerpo JSON
-  if (req.body != undefined) {
-
-    //Comprueba si están los campos obligatorios, que son el usuario y la contraseña y comprueban si coinciden
-    if (req.body.user === undefined || req.body.password === undefined) {
-      res.status(STATUS_BADFORMAT).end();
-    } else if (req.body.user === "user" && req.body.password === "1234") {
-      res.status(STATUS_OK).end();
-    } else {
-      res.status(STATUS_UNAUTHORIZED).end();
+  if (req.body != undefined) { //Comprueba si la petición tiene cuerpo
+    if (req.body.user === undefined || req.body.password === undefined)
+    {
+      res.status(STATUS_BADFORMAT).end(); //Si falta user o password devuelve error 400
+    } 
+    else if (req.body.user === "user" && req.body.password === "1234") 
+    {
+      res.status(STATUS_OK).end(); //Si son correctas devuelve 200
+    } 
+    else
+    {
+        res.status(STATUS_UNAUTHORIZED).end(); //Si no coinciden devuelve 401
     }
-  } else {
-    res.status(STATUS_BADFORMAT).end();
+  } 
+  else
+  {
+    res.status(STATUS_BADFORMAT).end(); //Devuelve 400 si no tiene cuerpo
   }
-});
-//User:user y password:1234
+
+});//User:user y password:1234
+
+
 
 // Funcion para comprobar que el cliente ha enviado datos
 const vFlecha = (req, res, next) => {
-  if (req.body !== undefined) {
+  if (req.body !== undefined) 
+  {
     next();
-  } else {
+  } 
+  else 
+  {
     res.status(STATUS_BADFORMAT).end();
   }
 };
 
 
+
+
  //TAREA 4. CRUD COMPLETO 
  // POST /voluntarios - Crear usuario
 app.post("/voluntarios", vFlecha, function (req, res) {
-  const client = new MongoClient(DB_URL);
+  const client = new MongoClient(DB_URL); //Se crea un cliente para conectarse a Mongo
   async function run() {
     try {
       //crea cliente
-      const db = client.db(DB_NAME);
+      const db = client.db(DB_NAME);                          //Seleccion de base de datos y colección
       const voluntarios = db.collection(DB_USERS_COLLECTION);
         // Busca si ya existe un usuario con el mismo nombre de usuario
       const buscado = await voluntarios.countDocuments({ user: req.body.user });
@@ -82,26 +98,26 @@ app.post("/voluntarios", vFlecha, function (req, res) {
         res.status(STATUS_CREATED).json({ _id: result.insertedId });
       }
     } finally {
-      //cierra conexión mongo
+      //cierra conexión mongo, se ejecuta siempre
       await client.close();
     }
   }
-  run().catch((ex) => {
+  run().catch((ex) => { //Si ocurre un error inesperado lo muestra por pantalla
     console.error("[SERVIDOR] POST /voluntarios: " + ex.toString());
     res.status(STATUS_SERVER_ERROR).end();
   });
 });
 
-// GET /voluntarios - Leer usuarios
+// GET /voluntarios - Leer todos los usuarios
 app.get("/voluntarios", function (req, res) {
   const client = new MongoClient(DB_URL);
   async function run() {
     try {
-      const db = client.db(DB_NAME);
+      const db = client.db(DB_NAME);                          //Selecciona la base y la colección
       const voluntarios = db.collection(DB_USERS_COLLECTION);
-      let options = new Object();
-      req.query.age !== undefined ? options.age = { $gte: parseInt(req.query.age) } : undefined;
-      req.query.limit !== undefined ? options.limit = parseInt(req.query.limit) : undefined;
+      let options = new Object(); //Crea un objeto vacío para guardar opciones
+      req.query.age !== undefined ? options.age = { $gte: parseInt(req.query.age) } : undefined; //Filtra usuarios entre un rango de edad
+      req.query.limit !== undefined ? options.limit = parseIntreq.query.limit) : undefined;     //Limita cuantos usuarios se muestran segun el valor de limit
      
       const cursor = await voluntarios.find({}, options);
       const result = await cursor.toArray();
@@ -117,20 +133,20 @@ app.get("/voluntarios", function (req, res) {
 });
 
 // GET /voluntarios/:id - Leer un usuario específico
-app.get("/voluntarios/:id", function (req, res) {
-  const client = new MongoClient(DB_URL);
+app.get("/voluntarios/:id", function (req, res) { //Permite localizar a un usuario por su id
+  const client = new MongoClient(DB_URL); 
   async function run() {
     try {
       const db = client.db(DB_NAME);
       const voluntarios = db.collection(DB_USERS_COLLECTION);
-      const id = new ObjectId(req.params.id);
+      const id = new ObjectId(req.params.id);  //Guarda el usuario que viene en la URL
       const result = await voluntarios.findOne({ _id: id });
       if (result) {
         res.json(result);
       } else {
-        res.status(STATUS_NOTFOUND).end();
+        res.status(STATUS_NOTFOUND).end();   //Si no existe devuelve error 404
       }
-    } catch (error) {
+    } catch (error) {  //si no tiene un formato valido lanza un error
       if (error instanceof Error && error.name === "BSONError") {
         res.status(STATUS_BADFORMAT).json({ message: "Formato de id inválido" });
       } else {
@@ -143,9 +159,11 @@ app.get("/voluntarios/:id", function (req, res) {
   run().catch(() => res.status(STATUS_SERVER_ERROR).end());
 });
 
-// PUT /voluntarios/:id - Actualizar usuario
+
+
+// PUT /voluntarios/:id - Modifica usuario
 app.put("/voluntarios/:id", function (req, res) {
-  const client = new MongoClient(DB_URL);
+  const client = new MongoClient(DB_URL); //Convierte URL en objeto
   async function run() {
     try {
       const db = client.db(DB_NAME);
@@ -171,6 +189,8 @@ app.put("/voluntarios/:id", function (req, res) {
   }
   run().catch(() => res.status(STATUS_SERVER_ERROR).end());
 });
+
+
 
 // DELETE /voluntarios/:id - Borrar voluntarios
 app.delete("/voluntarios/:id", function (req, res) {
